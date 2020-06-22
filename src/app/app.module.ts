6eca2df3248ff394f1c3726f25462eaa44e1922f
Paulo Teixeira <pauloteixeirajr@outlook.com>
2020-06-22 20:02:01 +0100

    adding application-wide styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GoalComponent } from './goal/goal.component';
+import { QuoteComponent } from './quote/quote.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GoalComponent
+    GoalComponent,
+    QuoteComponent
   ],
   imports: [
     BrowserModule,
