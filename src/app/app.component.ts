27435bec22790363edefcc2da5a0b1354e82d62d
Paulo Teixeira <pauloteixeirajr@outlook.com>
2020-06-22 19:48:51 +0100

    adding classes dynamically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ng-animations';
+  courseGoals = [
+    { title: 'Master Angular Styling', isActiveGoal: true },
+    { title: 'Understand Angular Animations', isActiveGoal: false },
+    { title: 'Master Angular Animations', isActiveGoal: false },
+  ];
 }
---
+++ src/app/goal/goal.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-goal',
+  templateUrl: './goal.component.html',
+  styleUrls: ['./goal.component.css'],
+})
+export class GoalComponent implements OnInit {
+  @Input() goal: any;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
