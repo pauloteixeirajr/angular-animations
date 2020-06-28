965c3854670ba7fc2ab9b687ec1661a544d79968
Paulo Teixeira <pauloteixeirajr@outlook.com>
2020-06-28 14:05:46 +0100

    using the query() method

--- src/app/new-features/new-features.component.ts
+++ src/app/new-features/new-features.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
-import { animate, style, transition, trigger } from '@angular/animations';
+import {
+  animate,
+  style,
+  transition,
+  trigger,
+  query,
+  group,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-new-features',
@@ -8,11 +15,29 @@ import { animate, style, transition, trigger } from '@angular/animations';
   animations: [
     trigger('panelState', [
       transition(':enter', [
-        style({
-          opacity: 0,
-          transform: 'translateX(100%)',
-        }),
-        animate(200),
+        group([
+          query('.card-header', [
+            style({
+              transform: 'translateY(-300px)',
+              opacity: 0,
+            }),
+            animate(300),
+          ]),
+          query('.card-body', [
+            style({
+              transform: 'translateX(-300px)',
+              opacity: 0,
+            }),
+            animate(300),
+          ]),
+          query('.card-footer', [
+            style({
+              transform: 'translateY(300px)',
+              opacity: 0,
+            }),
+            animate(300),
+          ]),
+        ]),
       ]),
       transition(':leave', [
         animate(
